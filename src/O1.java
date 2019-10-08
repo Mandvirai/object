c3f8c6f9ebaf05b7b398859fab1186012b927e5f
mandvi.rai_cs18@gla.ac.in <mohini7052030584>
2019-10-08 14:32:05 +0530

    Initial commit

---
+++ src/O1.java
@@ -0,0 +1,14 @@
+public class O1 {
+    protected int roll_no = 20;
+    protected String name = "Mandvi";
+
+    void O1Detail(){
+        System.out.println("name is:" + name);
+        System.out.println("roll_no");
+
+
+    }
+
+
+    }
+
---
+++ src/O2.java
@@ -0,0 +1,26 @@
+public class O2 {
+    public static void main(String[] args){
+        O1 stud1 = new O1();
+        O1 stud2 = new O1();
+        O1 stud3 = new O1();
+        stud1.roll_no = 30;
+        stud1.name = "Pragya";
+        stud2.roll_no = 22;
+        stud2.name =  "Himanshu";
+
+        System.out.println("roll no. is:" + stud1.roll_no);
+        System.out.println("name is:" + stud1.name);
+
+        System.out.println("roll no. is:" + stud2.roll_no);
+        System.out.println("name is:" + stud2.name);
+
+        System.out.println("roll no. is:" + stud3.roll_no);
+        System.out.println("name is:" + stud3.name);
+
+
+
+
+
+
+    }
+}
